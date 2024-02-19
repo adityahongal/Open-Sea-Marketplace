f0c7378336bcf175be1cbf80945fafd283fa4b49
adityahongal <adityahongalwork@gmail.com>
2024-02-19 20:07:21 +0530

    Added Cards Carousel temp

--- src/components/Home/Landing.jsx
+++ src/components/Home/Landing.jsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, Flex } from "@chakra-ui/react";
 import { Text, Image, Stack, Heading } from "@chakra-ui/react";
-import { Autoplay } from "swiper";
+import { Autoplay, Grid } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -8,15 +8,26 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "swiper/css/grid";
 
 // import required modules
 import { Pagination, Navigation } from 'swiper';
 
+// import style from CSS
+import "../../Assets/Landing.css";
+
 const Landing = () => {
   return (
+    // <Flex alignItems='center'>
     <Swiper
-        spaceBetween={50}
-        centeredSlides={true}
+        slidesPerView="auto"
+        spaceBetween={5}
+        grid={{
+          rows: 1,
+        }}
+        
+        centeredSlides={false}
+        observer={true}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
@@ -25,17 +36,18 @@ const Landing = () => {
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
+        modules={[Autoplay, Pagination, Navigation, Grid]}
         className="mySwiper"
       >
-    <Flex spacing={4}>
+    
     <SwiperSlide>
     <Card maxW="sm" minw="200" _hover={{ background: "red.100", cursor: "pointer" }}>
       <CardBody>
         <Image
-          src="https://i.seadn.io/s/raw/files/0a0bf8ac84328fd6d1a56442999a0567.jpg?auto=format&dpr=1&w=384"
+          src="https://i.seadn.io/s/raw/files/0a0bf8ac84328fd6d1a56442999a0567.jpg"
           alt="sequels"
           borderRadius="lg"
+          objectFit={"cover"}
         />
         <Stack mt="6" spacing="3">
           <Heading size="md">
@@ -53,7 +65,7 @@ const Landing = () => {
       <Card maxW="sm" minw="200" _hover={{ background: "red.100", cursor: "pointer" }}>
       <CardBody>
         <Image
-          src="https://i.seadn.io/s/raw/files/a24b23968a93875707c783dba76741b3.png?auto=format&dpr=1&w=384"
+          src="https://i.seadn.io/s/raw/files/a24b23968a93875707c783dba76741b3.png"
           alt="contra boy"
           borderRadius="lg"
         />
@@ -73,7 +85,7 @@ const Landing = () => {
     <Card maxW="sm" minw="200" _hover={{ background: "red.100", cursor: "pointer" }}>
       <CardBody>
         <Image
-          src="https://i.seadn.io/s/raw/files/10e521accd2247793bd64886a7136f45.jpg?auto=format&dpr=1&w=384"
+          src="https://i.seadn.io/s/raw/files/10e521accd2247793bd64886a7136f45.jpg"
           alt="justin odisho"
           borderRadius="lg"
         />
@@ -93,7 +105,7 @@ const Landing = () => {
     <Card maxW="sm" minw="200" _hover={{ background: "red.100", cursor: "pointer" }}>
       <CardBody>
         <Image
-          src="https://i.seadn.io/s/raw/files/6d460bf95261c56d99fee6d64eb031c0.jpg?auto=format&dpr=1&w=384"
+          src="https://i.seadn.io/s/raw/files/6d460bf95261c56d99fee6d64eb031c0.jpg"
           alt="Flower girls"
           borderRadius="lg"
         />
@@ -108,9 +120,48 @@ const Landing = () => {
       </CardBody>
     </Card>
     </SwiperSlide>
-    
-    </Flex>
+
+    <SwiperSlide> 
+    <Card maxW="sm" minw="200" _hover={{ background: "red.100", cursor: "pointer" }}>
+      <CardBody>
+        <Image
+          src="https://i.seadn.io/s/raw/files/6d460bf95261c56d99fee6d64eb031c0.jpg"
+          alt="Flower girls"
+          borderRadius="lg"
+        />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">
+            Flower girls #9343
+            </Heading>
+          <Text color="blue.600" fontSize="1xl">
+            Floor: 0.055 ETH
+          </Text>
+        </Stack>
+      </CardBody>
+    </Card>
+    </SwiperSlide>
+
+    <SwiperSlide> 
+    <Card maxW="sm" minw="200" _hover={{ background: "red.100", cursor: "pointer" }}>
+      <CardBody>
+        <Image
+          src="https://i.seadn.io/s/raw/files/6d460bf95261c56d99fee6d64eb031c0.jpg"
+          alt="Flower girls"
+          borderRadius="lg"
+        />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">
+            Flower girls #9343
+            </Heading>
+          <Text color="blue.600" fontSize="1xl">
+            Floor: 0.055 ETH
+          </Text>
+        </Stack>
+      </CardBody>
+    </Card>
+    </SwiperSlide>
     </Swiper>
+    // </Flex>
   );
 };
 
